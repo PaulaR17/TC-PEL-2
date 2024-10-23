e631d2fc38937bff8d5ad731b21f8f2d929f2f83
PaulaR17 <paularomerogallart@gmail.com>
2024-10-23 21:54:51 +0200

    Subiendo el proyecto de clase

---
+++ main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+/*
+Un centro de formación profesional está intentando desarrollar una aplicación que
+permita gestionar datos de los estudiantes. Para ello, necesitan un sistema en el que
+los usuarios (estudiantes o profesores) puedan trabajar los siguientes recursos:*
+*/
+
+
+/*
+Gestionar a los estudiantes de cada aula:
+Si es rol profe → almacenar y visualizar datos de sus estudiantes.Para ello se utilizará un almacén personal asociado a cada profesor.
+Un profesor solo puede tener una clase y un estudiante solo puede estar asociado a un profesor.
+Los datos que se deben ver de sus alumnos son: Nombre, apellido y notas por asignatura. Los datos que se deben ver de la clase son: Número de inscritos, listado de estudiantes.
+*/
+//PUNTEROS A TODO
+class Professor{
+    private:
+    int id;
+    string name;
+    string subject;
+    Student students[]
+
+    public:
+    i
+}
+
+cl
+
+/*
+Mochila de “material digital”.
+Rol de “Estudiante”
+El estudiante debe poder tener una mochila de “material digital”:
+Almacenará “documentos” de actividades realizadas, proyectos entregados, etc.
+Un documento, será un objeto “fichero” cargado en la mochila, que leerá del disco un fichero.txt con información del estudiante.
+El alumno podrá cargar en la mochila varios “ficheros” de datos, y podrá ver su contenido en la aplicación.
+*/
+
+/*
+Calculadora avanzada:
+Se debe incluir una funcionalidad de calculadora, que aparte de realizar operaciones básicas, también debe poder operar con matrices de n elementos, pudiendo ser las operaciones de suma, resta, multiplicación y división, calcular la matriz traspuesta a la dada, y, en caso de ser una matriz cuadrática, obtener su determinante.
+*/
+
+/*
+Sistema de altas y bajas de usuarios.
+“Administrador”.
+Dicho sistema debe poder manejarse los datos de los usuarios, de forma que se les pueda dar de alta o baja a cada usuario.
+Se les debe poder gestionar los datos del nombre, el apellido, y numero de usuario.
+Cada usuario tendrá un id irrepetible.
+Los usuarios deberán eliminarse de memoria cuando se den de baja.
+*/
+
+/*
+Un poco de diversión
+El sistema debe generar un número aleatorio entre 1 y 100
+El usuario tiene que adivinarlo.
+Cada vez que el usuario introduce un valor, el sistema debe indicar si el valor es mayor o menor que el introducido.
+Cuando el usuario adivine el valor, debe imprimir por pantalla el número de veces que inentó adivinarlo.
+*/
+
